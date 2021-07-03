265bf815b03dbff582c5596772bcbd991af46358
SawPyaeHtun <sawpyaehtun@tamronnetwork.com>
2021-07-03 08:40:33 +0630

    Initial Commit

---
+++ iOSUITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSUITest
+//
+//  Created by Tamron iMac 001 on 7/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
